0992aabbedb36e430af5c59f8126fe35d668b9f4
AlexV <a.visan@student.fontys.nl>
2021-04-15 09:56:27 +0200

    correctly displaying departments, reset button, confirmation for changing user info

--- website/index.php
+++ website/index.php
@@ -47,15 +47,37 @@ include_once ('Includes/dbh.inc.php');
 						$user->set_position($row['Position']);
 						$user->set_certificates($row['Certifications']);
 						$user->set_languages($row['Languages']);
-                        //$user->set_department($row['Department'])
+                        //$user->set_department($row['Department']);
 						$user->set_sdate($row['StartDate']);
 						$user->set_salary($row['Salary']);
-                        
-						
                         }                        
-                    }              
+                    }
+					
                
         ?>
+<?php
+$id = $_SESSION['userid'];
+                $sql = "SELECT Name FROM department INNER JOIN depemp ON depemp.EmpID = $id;";
+                //instantiate a new object of type prepared statement
+                $stmt = mysqli_stmt_init($conn); //uses whichever connection variable was used to connect to the db
+                //prepare the prepared statement, or rather try to parse the empty placeholder code first
+                if (!mysqli_stmt_prepare($stmt, $sql)) //good practice to check for errors with placeholder before binding values to it, but we don't inject data into the SQL statement in this block
+                {
+                   echo "SQL statement failed.";
+                }
+            
+                else
+                    
+                {
+                    //try and run the parameters inside the database
+                    mysqli_stmt_execute($stmt);
+                    $result = mysqli_stmt_get_result($stmt);//get the result from the query
+                    while($row = mysqli_fetch_assoc($result))
+                    {
+						$user->set_department($row['Name']);
+					}
+				}
+?>		
 <div class ="contCenter">
 <div class = "dashwrapper">
 <div class="containerdash">
--- website/updateinfo.php
+++ website/updateinfo.php
@@ -53,9 +53,34 @@ include_once ('Includes/dbh.inc.php');
 						$user->set_salary($row['Salary']);
 
                         }                        
-                    }              
+                    }
+					
+					
                
         ?>
+<?php
+$id = $_SESSION['userid'];
+                $sql = "SELECT Name FROM department INNER JOIN depemp ON depemp.EmpID = $id;";
+                //instantiate a new object of type prepared statement
+                $stmt = mysqli_stmt_init($conn); //uses whichever connection variable was used to connect to the db
+                //prepare the prepared statement, or rather try to parse the empty placeholder code first
+                if (!mysqli_stmt_prepare($stmt, $sql)) //good practice to check for errors with placeholder before binding values to it, but we don't inject data into the SQL statement in this block
+                {
+                   echo "SQL statement failed.";
+                }
+            
+                else
+                    
+                {
+                    //try and run the parameters inside the database
+                    mysqli_stmt_execute($stmt);
+                    $result = mysqli_stmt_get_result($stmt);//get the result from the query
+                    while($row = mysqli_fetch_assoc($result))
+                    {
+						$user->set_department($row['Name']);
+					}
+				}
+?>		
 <div class ="contCenter">
 <div class = "dashwrapper">
 <div class="containerdash">
@@ -81,7 +106,7 @@ include_once ('Includes/dbh.inc.php');
 			<b>Position:</b> <?php echo $user->get_position(); ?>
             <br>
 			<br>
-			<b>Department:</b> <?php //echo $user->get_department(); ?>
+			<b>Department:</b> <?php echo $user->get_department(); ?>
             <br>
 			<br>
 			<b>Employement date:</b> <?php echo $user->get_sdate(); ?>
@@ -118,12 +143,11 @@ include_once ('Includes/dbh.inc.php');
             <label for="emergencyc"><b>Emergency contact:</b></label>
             <input type="text" name="emergencyc" value = <?php echo $user->get_emergencyc(); ?>>
             <br>
-			<br>
-			<br>
-			<br>
-			<br>
 			<div class="updatebutton">
-            <button type = "submit" name = "submit" id = "btnupdate">Save changes</button>
+            <button type = "submit" name = "submit" id = "btnupdate" onclick="return  confirm('Are you sure you want to overwrite the current information?')">Save changes</button>
+			</div>
+			<div class="resetbutton">
+			<button type="reset" name = "reset" id = "btnreset" onclick="return  confirm('Are you sure you want to reset your changes?')">Reset</button>
 			</div>
             </form>
 </div>
